fefe45411f0302dd255a5bf3b4c673eede9579c0
Subham Srivastava <subham.srivastava@capgemini.com>
2026-08-21 17:43:30 +0530

    Update tabcards.js

--- blocks/tabcards/tabcards.js
+++ blocks/tabcards/tabcards.js
@@ -1,10 +1,5 @@
 export default async function decorate(block) {
     const rows = [...block.children];
-    console.log("rows", rows);
-    // Extract the text content safely
-    // const firstTexts = rows.map(row => row.children[0]?.innerText || "");
-
-    // console.log(firstTexts);
 
     const tabTitles = [...document.querySelectorAll('.block')]
         .map(block => block.children[0]?.textContent.trim());
