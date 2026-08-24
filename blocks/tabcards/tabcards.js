1156e2fcc78eed5aef0b3b1372e4528930935868
Subham Srivastava <subham.srivastava@capgemini.com>
2026-08-24 20:17:41 +0530

    Update tabcards.js

--- blocks/tabcards/tabcards.js
+++ blocks/tabcards/tabcards.js
@@ -93,7 +93,7 @@ export default async function decorate(block) {
             tabPanel.classList.add('active');
         });
     });
-    block.replaceWith(tabContainer);
-   // block.append(tabContainer);
+    //block.replaceWith(tabContainer);
+    block.prepend(tabContainer);
 
 }
