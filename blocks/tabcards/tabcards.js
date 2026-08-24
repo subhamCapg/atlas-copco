d5f313b426617faa18289e5162afec2d0ebbf2a7
Subham Srivastava <subham.srivastava@capgemini.com>
2026-08-24 17:34:54 +0530

    Update tabcards.js

--- blocks/tabcards/tabcards.js
+++ blocks/tabcards/tabcards.js
@@ -1,9 +1,35 @@
 export default async function decorate(block) {
     const rows = [...block.children];
-    
-    const tabTitles = [...document.querySelectorAll('.block')]
-        .map(block => block.children[0]?.textContent.trim());
 
-    console.log(tabTitles);
+    const tabTitlesRow = rows[0];
+    const cardRows = rows.slice(1); // all divs except first one
+    const tabs = [...tabTitlesRow.children].map(
+        (tab) => tab.textContent.trim(),
+    );
+
+    console.log(tabs);
+    const cards = cardRows.map((card) => {
+        const cells = [...card.children];
+
+        return {
+            image: cells[0]?.querySelector('picture'),
+            title: cells[1]?.textContent.trim(),
+            description: cells[2]?.textContent.trim(),
+            tabName: cells[3]?.textContent.trim(),
+        };
+    });
+    const groupedCards = {};
+
+    cards.forEach((card) => {
+        if (!groupedCards[card.tabName]) {
+            groupedCards[card.tabName] = [];
+        }
+
+        groupedCards[card.tabName].push(card);
+    });
+
+    console.log(groupedCards);
+
+
 
 }
