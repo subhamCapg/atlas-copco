1728538a494c88c7330c2bf31aacf3a26375d8b4
Subham Srivastava <subham.srivastava@capgemini.com>
2026-08-25 12:12:53 +0530

    Update tabcardsv2.js

--- blocks/tabcardsv2/tabcardsv2.js
+++ blocks/tabcardsv2/tabcardsv2.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
     const rows = [...block.children];
     console.log('Rows:', rows);
-    const spreadsheetPath = rows[0].textContent.trim();
+    const spreadsheetPath = rows[0].textContent.trim().pop();
     const response=fetch(`${spreadsheetPath}.json`).then((response) => response.json()).then((data) => {
         console.log('Data:', data);
         });
