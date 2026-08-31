213af83d7a26f5f90318fbfffa4a5162d55d30a4
Subham Srivastava <subham.srivastava@capgemini.com>
2026-08-31 12:28:12 +0530

    Update buttons.js

--- blocks/buttons/buttons.js
+++ blocks/buttons/buttons.js
@@ -1,3 +1,27 @@
-export async function decorate(block) { 
-  block.innerHTML = '<div class="button cmp-button--primary"><button id="button-5487aab983" type="button" class="cmp-button"><span class="cmp-button__text">Button</span></button></div>'; 
+export async function decorate(block) {
+  // Clear any existing content inside the block
+  
+  block.textContent = '';
+
+  // Create elements
+  const wrapperDiv = document.createElement('div');
+  const button = document.createElement('button');
+  const textSpan = document.createElement('span');
+
+  // Configure wrapper div
+  wrapperDiv.className = 'button cmp-button--primary';
+
+  // Configure button
+  button.id = 'button-5487aab983';
+  button.type = 'button';
+  button.className = 'cmp-button';
+
+  // Configure text span
+  textSpan.className = 'cmp-button__text';
+  textSpan.textContent = 'Button';
+
+  // Assemble the hierarchy
+  button.append(textSpan);
+  wrapperDiv.append(button);
+  block.append(wrapperDiv);
 }
